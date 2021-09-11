03a907682174a6532219f7e7e6535b23d974b2db
racyriaz <racyriaz@gmail.com>
2021-09-11 09:41:01 +0530

    admin site ready

--- aelo_backend/bank_api/admin.py
+++ aelo_backend/bank_api/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import *
 
 admin.site.register(User, UserAdmin)
+
+
+@admin.register(BankTranscations)
+class BankTransactions(admin.ModelAdmin):
+    list_display = ('id', 'user', 'amount', 'type_of_trans', 'cat_of_trans',
+                    'trans_hour', 'created', 'modified', 'payment_mode')
+    list_filter = ('type_of_trans', 'cat_of_trans', 'created', 'payment_mode')
