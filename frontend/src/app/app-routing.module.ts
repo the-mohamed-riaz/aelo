4514450cc68bacad1acce3fa1f047cc7f7a5c1bd
racyriaz <racyriaz@gmail.com>
2021-09-16 20:43:09 +0530

    lazy loading transaction and analytics module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'transaction', loadChildren: () => import('./transaction/transaction.module').then(m => m.TransactionModule) },
+  { path: 'analytics', loadChildren: () => import('./analytics/analytics.module').then(m => m.AnalyticsModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
