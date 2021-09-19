f18808ddee67efb394bf166414084cd37a097d54
racyriaz <racyriaz@gmail.com>
2021-09-19 18:30:34 +0530

    navbar layouts

---
+++ frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'r-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
