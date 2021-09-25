9e81711bcdabe098cf97277bdc9ff3e71c3dca41
racyriaz <racyriaz@gmail.com>
2021-09-25 11:51:35 +0530

    dropdown grouped

--- frontend/src/app/dropdown/semantic-dd/semantic-dd.component.ts
+++ frontend/src/app/dropdown/semantic-dd/semantic-dd.component.ts
@@ -12,6 +12,7 @@ export class SemanticDdComponent implements OnInit {
   options: Array<dropdown_i> | [] = [];
   @Input() dd_name!: string;
   @Input() id_: string = "XD";
+  @Input() placeHolder?: string;
 
   constructor(private constant: DropdownsService) {
 
@@ -22,12 +23,15 @@ export class SemanticDdComponent implements OnInit {
     switch (this.dd_name) {
       case "trans_choice":
         this.options = this.constant.trans_choice;
+        this.placeHolder = "Debit/Credit";
         break;
       case "trans_category":
         this.options = this.constant.trans_category;
+        this.placeHolder = "Category of Transaction";
         break;
       case "payment_modes":
         this.options = this.constant.payment_modes;
+        this.placeHolder = "Payment Mode";
         break;
 
       default:
