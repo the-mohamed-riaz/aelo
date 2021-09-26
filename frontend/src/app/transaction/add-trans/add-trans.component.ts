fb9ef473da38672ae9d2d8fba0b0cb17e70cbca6
racyriaz <racyriaz@gmail.com>
2021-09-26 10:50:52 +0530

    3rd dropdown

--- frontend/src/app/transaction/add-trans/add-trans.component.ts
+++ frontend/src/app/transaction/add-trans/add-trans.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTransComponent implements OnInit {
 
-  constructor() { }
+  randId: string;
+  constructor() {
+    this.randId = 'id_' + this.gen_RandId();
+  }
 
   ngOnInit(): void {
   }
 
+  gen_RandId() {
+    let id = Math.random() * 1000;
+    return Math.round(id)
+  }
+  logThis(val: string) {
+    console.log(val);
+  }
 }
