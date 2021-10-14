a1bcfe40c1ff1780eea8e086784b158cffd48a4a
racyriaz <racyriaz@gmail.com>
2021-10-14 14:20:28 +0530

    grid layout for trans home

--- frontend/src/app/transaction/recent-transaction/recent-transaction.component.ts
+++ frontend/src/app/transaction/recent-transaction/recent-transaction.component.ts
@@ -12,8 +12,11 @@ export class RecentTransactionComponent implements OnInit {
 
   constructor() {
     this.data = [
-      { icon: 'phone volume', comment: 'phone', timeStamp: '15 Sep at 4:10 PM', amount: '4500' },
-      { icon: 'tty', comment: 'clock', timeStamp: '15 Sep at 4:10 PM', amount: '4500' },
+      { icon: 'phone volume', comment: 'phone', timeStamp: '15 Sep at 3:57 PM', amount: '4500' },
+      { icon: 'tty', comment: 'clock', timeStamp: '15 Sep at 4:00 PM', amount: '3200' },
+      { icon: 'clock', comment: 'new watch', timeStamp: '15 Sep at 4:05 PM', amount: '5000' },
+      { icon: 'home', comment: 'home', timeStamp: '15 Sep at 4:17 PM', amount: '1480' },
+      { icon: 'man', comment: 'friend', timeStamp: '15 Sep at 4:39 PM', amount: '400' },
     ]
   }
 
