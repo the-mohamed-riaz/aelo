de9fa7200ae8eea56d4c1aa07b40b7805310f3f4
racyriaz <racyriaz@gmail.com>
2021-12-05 19:02:29 +0530

    recent transaction list works

--- frontend/src/app/transaction/models/model.ts
+++ frontend/src/app/transaction/models/model.ts
@@ -1,6 +1,17 @@
-export interface recentTransaction {
+export interface $pretty_recent_trans {
     icon: string,
     comment: string,
     timeStamp: string,
     amount: string
+}
+
+export interface $recentTransaction {
+    id: string;
+    amount: string;
+    type_of_trans: string;
+    cat_of_trans: string;
+    trans_date: string;
+    trans_hour: string;
+    payment_mode: string;
+    user: string;
 }
--- frontend/src/app/transaction/recent-transaction/recent-transaction.component.ts
+++ frontend/src/app/transaction/recent-transaction/recent-transaction.component.ts
@@ -1,27 +1,65 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { recentTransaction } from '../models/model';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { $pretty_recent_trans, $recentTransaction } from '../models/model';
 
 @Component({
   selector: 'recent-transaction',
   templateUrl: './recent-transaction.component.html',
   styleUrls: ['./recent-transaction.component.scss']
 })
+
 export class RecentTransactionComponent implements OnInit {
+  data: Array<$pretty_recent_trans> = [];
+  recent_trans_query: Array<$recentTransaction> = [];
+  user: string | null = null;
+  constructor(private http: HttpClient, private cookie: CookieService) {
+    if (this.cookie.check('username')) {
+      this.user = this.cookie.get('username');
+      this.fetchValues();
+      setInterval(async () => this.fetchValues(), 1000 * 30);
+    }
 
-  @Input() data: Array<recentTransaction>;
-
-  constructor() {
-    this.data = [
-      { icon: 'phone volume', comment: 'phone', timeStamp: '15 Sep at 3:57 PM', amount: '4500' },
-      { icon: 'tty', comment: 'clock', timeStamp: '15 Sep at 4:00 PM', amount: '3200' },
-      { icon: 'clock', comment: 'new watch', timeStamp: '15 Sep at 4:05 PM', amount: '5000' },
-      { icon: 'home', comment: 'home', timeStamp: '15 Sep at 4:17 PM', amount: '1480' },
-      { icon: 'man', comment: 'friend', timeStamp: '15 Sep at 4:39 PM', amount: '400' },
-    ]
   }
 
-  ngOnInit(): void {
+  fetchValues() {
+    this.http.get<Array<$recentTransaction>>(`http://localhost:8000/recent/?username=${this.user}`).subscribe(
+      (val) => {
+        this.recent_trans_query = val;
+        for (let item of this.recent_trans_query) {
+          let row: $pretty_recent_trans = {
+            amount: "",
+            comment: "",
+            icon: "",
+            timeStamp: ""
+          };
+          if (item.payment_mode === "cash") {
+            row.icon = "rupee sign";
+          } else if (item.payment_mode === "upi") {
+            row.icon = "user outline";
+          } else if (item.payment_mode === "bank_transfer") {
+            row.icon = "university";
+          } else if (item.payment_mode === "cheque") {
+            row.icon = "bookmark outline";
+          } else if (item.payment_mode === "atm_card") {
+            row.icon = "credit card outline";
+          } else if (item.payment_mode === "others") {
+            row.icon = "handshake outline";
+          }
+
+          row.comment = item.cat_of_trans;
+          row.timeStamp = item.trans_date + "  " + item.trans_hour;
+          row.amount = item.amount;
+          if (this.data.length >= 5) {
+            this.data.shift();
+          }
+          this.data.push(row);
+        }
+      },
+      (err) => console.log("err while fetching recent trans:\n", err)
+    );
   }
 
+  ngOnInit(): void { }
 
 }
