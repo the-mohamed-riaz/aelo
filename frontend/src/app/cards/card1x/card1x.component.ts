2f0799368855109043c316986acd9210f767237c
racyriaz <racyriaz@gmail.com>
2021-12-15 21:58:52 +0530

    bank balance updates in frontend dynamically

--- frontend/src/app/cards/card1x/card1x.component.ts
+++ frontend/src/app/cards/card1x/card1x.component.ts
@@ -4,6 +4,13 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, Input, Injectable } from '@angular/core';
 import { FormDataGeneratorService } from 'src/app/shared/form-data-generator.service';
 
+export interface bank_api_resp {
+  account_balance: number;
+  bank_name: string;
+  timestamp: string;
+  username: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -18,17 +25,22 @@ export class Card1xComponent implements OnInit {
   provided_bk_details = false;
   username: string;
 
+  bank_balance: number | null | string = null;
+
   constructor(private http: HttpClient, private cookie: CookieService, private generator: FormDataGeneratorService) {
     this.username = this.cookie.get('username');
     this.bank_form.controls['username'].setValue(this.username);
-    this.get_bk_details()
+    this.get_bk_details();
   }
 
   get_bk_details() {
-    this.http.get(`http://localhost:8000/account-balance/?username=${this.username}`).subscribe(
-      val => {
+    this.http.get<bank_api_resp>(`http://localhost:8000/account-balance/?username=${this.username}`).subscribe(
+      (val: bank_api_resp) => {
         console.debug("val from bank details api", val);
         val ? this.provided_bk_details = true : this.provided_bk_details = false;
+        if (this.provided_bk_details) {
+          this.bank_balance = val.account_balance.toLocaleString();
+        }
       },
       err => {
         console.debug("error from bank api", err);
@@ -43,6 +55,7 @@ export class Card1xComponent implements OnInit {
   })
 
   ngOnInit(): void {
+    setInterval(() => this.get_bk_details(), 3000);
   }
 
   submitForm() {
