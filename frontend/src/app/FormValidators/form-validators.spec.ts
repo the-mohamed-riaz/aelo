81993bcf1654fc4d4c3027547bf4162299a09685
racyriaz <racyriaz@gmail.com>
2021-12-25 19:40:37 +0530

    setting backend

--- frontend/src/app/FormValidators/form-validators.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FormValidators } from './form-validators';
-
-describe('FormValidators', () => {
-  it('should create an instance', () => {
-    expect(new FormValidators()).toBeTruthy();
-  });
-});
