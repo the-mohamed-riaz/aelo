d1602663c48c736c27e3e396035df2bf65852823
racyriaz <racyriaz@gmail.com>
2021-12-26 15:45:53 +0530

    settings postgres

--- aelo_backend/settings.py
+++ aelo_backend/settings.py
@@ -95,14 +95,14 @@ WSGI_APPLICATION = 'aelo_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-# }
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+    }
+}
 
 # custom auth model:
 AUTH_USER_MODEL = 'bank_api.User'
