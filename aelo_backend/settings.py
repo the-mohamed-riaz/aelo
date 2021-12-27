d0c0e1f49313e32467d7a2738b77961565490c7b
racyriaz <racyriaz@gmail.com>
2021-12-27 22:44:32 +0530

    double urls

--- aelo_backend/settings.py
+++ aelo_backend/settings.py
@@ -124,11 +124,11 @@ django_heroku.settings(locals())
 # DATABASES['default']=dj_database_url.config(conn_max_age=600)
 # DATABASES = { 'default': dj_database_url.config() }
 
-if ON_HEROKU:
+# if ON_HEROKU:
     # DATABASE_URL = 'postgresql://<postgresql>'
-    DATABASE_URL="postgres://ehakfkfplehpyk:860ec4b0aad858be87a5b500ecf2c08c50e41ed9a2e3472763acc7f3ead9fca2@ec2-54-173-2-216.compute-1.amazonaws.com:5432/d9dt8gf6l68n5q"
-else:
-    DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASE_URL="postgres://ehakfkfplehpyk:860ec4b0aad858be87a5b500ecf2c08c50e41ed9a2e3472763acc7f3ead9fca2@ec2-54-173-2-216.compute-1.amazonaws.com:5432/d9dt8gf6l68n5q"
+# else:
+    # DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
