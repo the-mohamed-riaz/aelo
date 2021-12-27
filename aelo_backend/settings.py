502475f9031e487403707466448b5a96f789e39f
racyriaz <racyriaz@gmail.com>
2021-12-27 20:08:06 +0530

    removed bla stuff

--- aelo_backend/settings.py
+++ aelo_backend/settings.py
@@ -98,8 +98,8 @@ WSGI_APPLICATION = 'aelo_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 DATABASES = {
     'default': {
