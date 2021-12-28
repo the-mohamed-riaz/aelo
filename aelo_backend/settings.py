a30f9cc571f4517f70c0df739397dd9284725b57
racyriaz <racyriaz@gmail.com>
2021-12-28 21:42:10 +0530

    migrations folder included

--- aelo_backend/settings.py
+++ aelo_backend/settings.py
@@ -104,13 +104,14 @@ WSGI_APPLICATION = 'aelo_backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
+django_heroku.settings(locals())
 
-# DATABASES = {
+DATABASES = {
 
-#     # "default": {
-#     #     "ENGINE" : "django.db.backends.sqlite3",
-#     #     "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-#     # }
+    "default": {
+        "ENGINE" : "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    }
 
 #     "default": {
 #         # "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -119,9 +120,8 @@ WSGI_APPLICATION = 'aelo_backend.wsgi.application'
 #         "HOST": "localhost", 
 #         "PORT": "5432",
 #     }
-# }
+}
 
-django_heroku.settings(locals())
 
 
 # DATABASE_URL="postgres://ehakfkfplehpyk:860ec4b0aad858be87a5b500ecf2c08c50e41ed9a2e3472763acc7f3ead9fca2@ec2-54-173-2-216.compute-1.amazonaws.com:5432/d9dt8gf6l68n5q"
@@ -137,7 +137,7 @@ django_heroku.settings(locals())
 # else:
     # DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+# DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
 
 # custom auth model:
