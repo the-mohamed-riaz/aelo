3492e83b79230e6e6aedb60e8f100b45c018f910
racyriaz <racyriaz@gmail.com>
2021-12-29 11:29:47 +0530

    login + register page responsiveness corrected

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:8000/"
+  // apiUrl: "http://localhost:8000/",
+  apiUrl: "https://aelo-backend-cs50.herokuapp.com/"
+
 };
